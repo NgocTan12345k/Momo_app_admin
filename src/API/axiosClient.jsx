d68a53e282a6ecd11686b28a9105b51ad3dac2d3
NgocTan12345k <kiemtienonlinee1@gmail.com>
2023-05-09 00:28:01 +0700

    fix axiosClient file

--- src/API/axiosClient.jsx
+++ src/API/axiosClient.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 const createAPI = () => {
   const APIInstance = axios.create({
     baseURL: "http://localhost:5005/api",
+    // baseURL: "https://momo-app-server.onrender.com/api",
   });
   return APIInstance;
 };
